20f294cb029aa0d8426640bcd0789d3dd003c5bd
Matthieu Tran <matthieuktran@gmail.com>
2018-12-31 02:29:07 -0800

    Add API usage

--- config/settings.example.py
+++ config/settings.example.py
@@ -1,3 +1,11 @@
 discord = {
-    'token': ""
+    "token": "",
+    "guild_id": ,
+    "upload_cid": ,
+    "category_id": 
+}
+
+API = {
+    "baseUrl": "",
+    "token": "",
 }
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import aiohttp
+import asyncio
 import discord
+import threading
 from config import *
+from discord.ext import commands
 
-client = discord.Client()
+client = commands.Bot(command_prefix = '!')
+
+headers = {"Authorization": "Bearer " + settings.API['token']}
+allowed = ['chibi1', 'chibi2', 'traditional', 'commissions', 'panels', 'other']
+
 
 @client.event
 async def on_ready():
@@ -9,13 +17,92 @@ async def on_ready():
     game = discord.Game("mungjar")
     stream = discord.Streaming(name="mungjar", url="https://twitch.tv/mungjar", twitch_name="mungjar")
     await client.change_presence(activity=stream)
+    await checkTasks()
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+
+    await process_uploads(message)
+    await client.process_commands(message)
+
     # if message.content == "Hello":
     #     await client.send_message(message.channel, "World")
+
+async def process_uploads(message):
+    if message.channel.id != settings.discord['upload_cid'] or len(message.attachments) < 1 or not message.content:
+        return
+
+    url = settings.API['baseUrl'] + '/pictures'
+
+    if message.content not in allowed:
+        return
+
+    for attachment in message.attachments:
+        test = await attachment.save("tempFile")
+        tempFile = open('tempfile', 'rb')
+
+        async with aiohttp.ClientSession() as session:
+            data = aiohttp.FormData()
+            data.add_field('file', open('tempFile', 'rb'))
+            data.add_field('type', message.content)
+
+            await session.post(url, data=data, headers=headers)
+        await message.add_reaction("\u2049")
+
+async def checkTasks():
+    while True:
+        async with aiohttp.ClientSession() as session:
+            url = settings.API['baseUrl'] + '/tasks'
+            response = await session.get(url, headers=headers)
+
+            tasks = (await response.json())['data']
+            for task in tasks:
+                commission = task['commission']
+                guild = client.get_guild(settings.discord['guild_id'])
+                member = guild.get_member(int(commission['discord']))
+                print(member)
+                category = guild.get_channel(settings.discord['category_id'])
+
+                # Create Role
+                newRole = await guild.create_role(name=commission['token'])
+                await member.add_roles(newRole)
+
+                # Create discord
+                overwrites = {
+                    guild.default_role: discord.PermissionOverwrite(read_messages=False),
+                    guild.me: discord.PermissionOverwrite(read_messages=True),
+                    newRole: discord.PermissionOverwrite(read_messages=True)
+                }
+
+                # Create Channel
+                newChannel = await guild.create_text_channel('commission-' + commission['token'], overwrites=overwrites, category=category)
+                await newChannel.send("Hello "+member.mention+". Please continue discussion about your commission here.\n"
+                                    + "Here are is the summary about your commission:\n"
+                                    + "**Name:** " + commission['name'] + '\n'
+                                    + "**Email:** " + commission['email'] + '\n'
+                                    + "**Date needed by:** " + commission['duedate'] + '\n'
+                                    + "**Type:** " + commission['type'] + '\n'
+                                    + "**Commercial Use?** " + str(commission['commercial']) + '\n'
+                                    + "**Additional Info** \n```\n" + commission['info'] + '```\n'
+                                    + "Your commission link can be found at:\n"
+                                    + "https://mungjar.aegyo.pro/commissions/"
+                                    + commission['token'])
+
+                # Mark completed
+                url = settings.API['baseUrl'] + '/tasks/' + str(task['id'])
+                data = aiohttp.FormData()
+                data.add_field('status', 1)
+
+                response = await session.patch(url, headers=headers, data=data)
+
+        await asyncio.sleep(20)
+
+
+@client.command(name='ping')
+async def ping(ctx):
+    await ctx.send('pong')
 
 
 client.run(settings.discord['token'])
